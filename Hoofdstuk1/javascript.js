4b38dd3fab1d7ddb608f43ba9e972a9cc1d495de
tim van der stam <timvanderstam@hotmail.com>
2019-11-21 10:50:20 +0100

    Hoofdstuk 1 afgemaakt

---
+++ Hoofdstuk1/javascript.js
@@ -0,0 +1,16 @@
+var a = 1;
+for (var i = 0;  i < 10 ;  i++) {
+    console.log(" de waarde van i is " +i)
+}
+if (a = 1) {
+    console.log("hello world");
+}
+
+var mijnauto = {
+    merk: "ford",
+    type:"modeo",
+    aantalwielen: 4,
+    kleur: "blauw"
+}
+
+console.log(mijnauto.kleur)
