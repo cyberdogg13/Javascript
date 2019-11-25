53ca349e1547af4cf4e8ff492bf74c786849ce31
tim van der stam <timvanderstam@hotmail.com>
2019-11-25 14:45:44 +0100

    Hoofdstuk 2 afgemaakt

---
+++ Hoofdstuk2/script.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "dit is de pagina titel";
+document.getElementById("subkop").innerText = "dit is de subkop";
+document.getElementById("banaan1").src = "banaan1.jfif";
+document.getElementById("banaan2").src ="banaan2.jfif";
---
+++ Hoofdstuk2/stijlaanpassen.js
@@ -0,0 +1,10 @@
+document.getElementById("bl1").style.backgroundColor ="lightblue";
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl1").style.paddingTop = "15px";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl2").style.marginTop = "15px";
+document.getElementById("bl2").style.backgroundColor ="green";
+document.getElementById("bl3").style.backgroundColor ="yellow";
+document.getElementById("bl3").style.marginTop = "30px";
+document.getElementById("bl3").style.textAlign = "right";
+document.getElementById("bl4").style.backgroundColor ="red";
